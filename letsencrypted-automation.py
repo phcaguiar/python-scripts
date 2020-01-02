1daf8376d52309a85c7404dea0ca642c9729078a
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2020-01-02 17:48:22 -0300

    update script

--- letsencrypted-automation.py
+++ letsencrypted-automation.py
@@ -4,14 +4,15 @@ from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from datetime import datetime  
 from datetime import timedelta
-import pprint, requests, base64, os, slack
+import requests, base64, os, slack
+
 def queue_build():
-    # Create a connection to the org
-    personal_access_token = (os.environ.get('personal_access_token'))
-    base_url = (os.environ.get('base_url'))
-    organization_name = (os.environ.get('organization_name'))
-    project_name = (os.environ.get('project_name'))
-    build_id = (os.environ.get('build_id'))
+
+    personal_access_token = os.environ.get('personal_access_token')
+    base_url = os.environ.get('base_url')
+    organization_name = os.environ.get('organization_name')
+    project_name = os.environ.get('project_name')
+    build_id = os.environ.get('build_id')
     fully_url = base_url + organization_name + '/' + project_name + '/_apis/build/builds?api-version=5.1'
     credentials = BasicAuthentication('', personal_access_token)
     connection = Connection(base_url=base_url + organization_name, creds=credentials)
@@ -29,24 +30,25 @@ def queue_build():
         'Accept': 'application/json'
     }
     requests.post(fully_url, json=params_dict, headers=head)
+
 def get_certdate():
-    # get azure credential
+
     credential = DefaultAzureCredential()
-    # get azure key vault certificate expiration date
     certificate_client = CertificateClient(
         vault_url=(os.environ.get('kvurl')), credential=credential)
     certificate = certificate_client.get_certificate(
         certificate_name=(os.environ.get('certname')))
     cert_date = certificate.properties.expires_on.strftime("%d/%m/%Y")
     limit_date = (datetime.now() + timedelta(days=7)).strftime("%d/%m/%Y")
-    slack_token = (os.environ.get('slacktoken'))
+    slack_token = os.environ.get('slacktoken')
+
     if cert_date == limit_date:
         print('O certificate vai expirar')
         queue_build()
         client = slack.WebClient(token=slack_token)
         client.chat_postMessage(
             channel="@pedro.aguiar",
-            username="Phcaguiar",
+            username="Finacial Bot SRE",
             text='O certificate vai expirar'
         )
     else:
@@ -55,7 +57,8 @@ def get_certdate():
         client = slack.WebClient(token=slack_token)
         client.chat_postMessage(
             channel="@pedro.aguiar",
-            username="Phcaguiar",
+            username="Finacial Bot SRE",
             text=("The certificate expires on", cert_date)
         )
+
 get_certdate()
